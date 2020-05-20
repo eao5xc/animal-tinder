efc7e9ebce98df57ea756d930420360745febe15
Emmanuel Ogunjirin <eao5xc@virginia.edu>
2020-05-20 11:01:58 -0400

    Implemented components

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { ThemeProvider } from "styled-components";
+// import Image from "material-ui-image";
+import Button from "@material-ui/core/Button";
 import { lightTheme, darkTheme } from "../Theme/Theme";
 import { GlobalStyles } from "../Theme/Global";
 
@@ -19,7 +21,18 @@ function App() {
     <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
       <div>
         <GlobalStyles />
-        <button onClick={toggleTheme}>Toggle theme</button>
+        <div>
+          <img alt="logo" src="https://source.unsplash.com/random" />
+        </div>
+        <Button color="inherit" onClick={toggleTheme}>
+          Dislike theme
+        </Button>
+        <Button color="inherit" onClick={toggleTheme}>
+          SuperLike theme
+        </Button>
+        <Button color="inherit" onClick={toggleTheme}>
+          Like theme
+        </Button>
         <h1>It's a {theme} theme!</h1>
       </div>
     </ThemeProvider>
