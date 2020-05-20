40354a5be702ffab0535c1f9c0c0a080ee3d5804
Emmanuel Ogunjirin <eao5xc@virginia.edu>
2020-05-20 13:39:33 -0400

    Auto stash before merge of "dev" and "frontend"

--- src/App/App.js
+++ src/App/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 
 import { createStore } from "redux";
@@ -6,11 +7,36 @@ import reducer from "../store/reducer";
 import { like, dislike, superLike } from "../store/actions";
 
 import Button from "@material-ui/core/Button";
+=======
+import React, { useState } from "react";
+import { ThemeProvider } from "styled-components";
+import ThumbDown from "@material-ui/icons/ThumbDownRounded";
+import ThumbUp from "@material-ui/icons/ThumbUpRounded";
+import Favorite from "@material-ui/icons/FavoriteRounded";
+import Button from "@material-ui/core/Button";
+import Box from "@material-ui/core/Box";
+import AnimalCard from "../Components/AnimalCard";
+import { lightTheme, darkTheme } from "../Theme/Theme";
+import { GlobalStyles } from "../Theme/Global";
+// The function that toggles between themes
+function App() {
+  const imageSource = "https://source.unsplash.com/random";
+
+  const [theme, setTheme] = useState("light");
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  };
+>>>>>>> frontend
 
 const store = createStore(reducer);
 
 export default function App() {
   return (
+<<<<<<< HEAD
     <Provider store={store}>
       <Content />
     </Provider>
@@ -57,6 +83,34 @@ const Content = () => {
             Like
           </Button>
         </div>
+=======
+    <ThemeProvider theme={theme === "light" ? darkTheme : lightTheme}>
+      <div>
+        <GlobalStyles />
+        <AnimalCard ImageSource={imageSource}></AnimalCard>
+        <Box component="span" m={8}></Box>
+        <ThumbDown
+          color="primary"
+          style={{ fontSize: 100 }}
+          onClick={toggleTheme}
+        ></ThumbDown>
+        <Box component="span" m={11}></Box>
+        <Favorite
+          color="secondary"
+          style={{ fontSize: 150 }}
+          onClick={toggleTheme}
+        ></Favorite>
+        <Box component="span" m={11}></Box>
+        <ThumbUp
+          color="primary"
+          style={{ fontSize: 100 }}
+          onClick={toggleTheme}
+        ></ThumbUp>
+        <Box component="span" m={20}></Box>
+        <Button color="secondary" onClick={toggleTheme}>
+          change to {theme} theme
+        </Button>
+>>>>>>> frontend
       </div>
     );
 
