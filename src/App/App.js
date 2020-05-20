a953f51952c901c7406df212d17400d95d0af837
Emmanuel Ogunjirin <eao5xc@virginia.edu>
2020-05-20 10:36:47 -0400

    Implemented toggle

--- src/App/App.js
+++ src/App/App.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useState } from "react";
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "../Theme/Theme";
+import { GlobalStyles } from "../Theme/Global";
 
+// The function that toggles between themes
 function App() {
+  const [theme, setTheme] = useState("light");
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  };
+
+  // Return the layout based on the current theme
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <div>
+        <GlobalStyles />
+        <button onClick={toggleTheme}>Toggle theme</button>
+        <h1>It's a light theme!</h1>
+      </div>
+    </ThemeProvider>
   );
 }
 
