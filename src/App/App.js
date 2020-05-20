f9e2ba41ea06c73bc3f2004318ea90ee18b2a1ac
Emmanuel Ogunjirin <eao5xc@virginia.edu>
2020-05-20 13:46:58 -0400

    Fixed Conflicts

--- src/App/App.js
+++ src/App/App.js
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-import React from "react";
-
+import React, { useState } from "react";
 import { createStore } from "redux";
 import { Provider, useDispatch, useSelector } from "react-redux";
 import reducer from "../store/reducer";
-import { like, dislike, superLike } from "../store/actions";
-
-import Button from "@material-ui/core/Button";
-=======
-import React, { useState } from "react";
 import { ThemeProvider } from "styled-components";
 import ThumbDown from "@material-ui/icons/ThumbDownRounded";
 import ThumbUp from "@material-ui/icons/ThumbUpRounded";
@@ -18,9 +11,12 @@ import Box from "@material-ui/core/Box";
 import AnimalCard from "../Components/AnimalCard";
 import { lightTheme, darkTheme } from "../Theme/Theme";
 import { GlobalStyles } from "../Theme/Global";
+
 // The function that toggles between themes
+
 function App() {
   const imageSource = "https://source.unsplash.com/random";
+  const store = createStore(reducer);
 
   const [theme, setTheme] = useState("light");
   const toggleTheme = () => {
@@ -30,60 +26,9 @@ function App() {
       setTheme("light");
     }
   };
->>>>>>> frontend
 
-const store = createStore(reducer);
-
-export default function App() {
+  // Return the layout based on the current theme
   return (
-<<<<<<< HEAD
-    <Provider store={store}>
-      <Content />
-    </Provider>
-  );
-}
-
-const Content = () => {
-  const pets = useSelector((state) => state);
-  const dispatch = useDispatch();
-
-  const handleDislike = () => {
-    dispatch(dislike());
-  };
-
-  const handleSuperLike = () => {
-    dispatch(superLike());
-  };
-
-  const handleLike = () => {
-    dispatch(like());
-  };
-
-  const noMoreProfiles = () => {
-    return pets.pendingPets.length === 0;
-  };
-  if (!noMoreProfiles())
-    return (
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <img alt="logo" src={pets.currentPet.image} width="50%" height="auto" />
-        <div>
-          <Button color="inherit" onClick={handleDislike}>
-            Dislike
-          </Button>
-          <Button color="inherit" onClick={handleSuperLike}>
-            SuperLike
-          </Button>
-          <Button color="inherit" onClick={handleLike}>
-            Like
-          </Button>
-        </div>
-=======
     <ThemeProvider theme={theme === "light" ? darkTheme : lightTheme}>
       <div>
         <GlobalStyles />
@@ -110,11 +55,14 @@ const Content = () => {
         <Button color="secondary" onClick={toggleTheme}>
           change to {theme} theme
         </Button>
->>>>>>> frontend
       </div>
-    );
+    </ThemeProvider>
+  );
+}
 
-  return (
+export default App;
+
+/* return (
     <div>
       {pets.pastPets.map((pet) => {
         return (
@@ -125,5 +73,4 @@ const Content = () => {
         );
       })}
     </div>
-  );
-};
+  ); */
